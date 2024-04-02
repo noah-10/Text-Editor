3aa3e585fc4281bc7dd5c5e004e652bc08c46eae
noah-10 <noah.fryman.10@gmail.com>
2024-04-01 22:46:55 -0400

    Added loaders, sw, and manifest to webpack. Also added CRUD database functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,45 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+  // Creates a connection to the database
+  const contactDb = await openDB('jate', 1);
+
+  // Create a new transaction with the database and the permission
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Open the desired object store
+  const store = tx.objectStore('jate');
+
+  // Store and pass in the content
+  const request = store.add({ text: content });
+
+  // get confirmation on the request
+  const result = await request;
+
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // Creates a connection to the database
+  const contactDb = await openDB('jate', 1);
+
+  // Create a new transaction with the database and the permission
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  // Open the desired object store
+  const store = tx.objectStore('jate');
+
+  // Get all data from database
+  const request = store.getAll();
+
+  // get confirmation on the request
+  const result = await request;
+
+  console.log('Data saved to the database', result);
+  return result
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,21 +9,70 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 module.exports = () => {
   return {
     mode: 'development',
+    // Entry point for files
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      header: './src/js/header.js',
+      editor: './src/js/editor.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Generates the html file and injects the bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+
+      // Injects the customer service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Creates a manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Text Editor",
+        short_name: "Editor",
+        description: "Practice and save your code!",
+        background_color: '#31a9e1',
+        theme_color: '#31a9e1',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
+      // CSS loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          user: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            },
+          },
+        },
       ],
     },
   };
